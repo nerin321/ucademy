6484e50463efbfe57166a5f118c52012c1778b1d
nerin321 <lethanhtuangam@gmail.com>
2024-12-20 14:30:51 +0700

    mông

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -14,7 +14,7 @@ export const connectToDatabase = async () => {
   }
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "nerin",
+      dbName: "ucademy",
     });
     isConnected = true;
     console.log("Using new database connection");
