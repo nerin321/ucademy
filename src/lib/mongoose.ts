61620433ef1d66229fea5b1c0fb9c55749ad4f07
nerin321 <lethanhtuangam@gmail.com>
2024-12-20 11:22:03 +0700

    change db name

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -14,7 +14,7 @@ export const connectToDatabase = async () => {
   }
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "ucademy",
+      dbName: "nerin",
     });
     isConnected = true;
     console.log("Using new database connection");
